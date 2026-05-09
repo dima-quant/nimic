2b44a7e00d9439d12604718913656c4d95d72529
Dima <dima@Dimas-Air.home>
2026-05-09 12:02:55 +0200

    PyObject alias

--- src/nimic/nimpy/__init__.py
+++ src/nimic/nimpy/__init__.py
@@ -6,3 +6,5 @@ brings in PyObject, RawPyBuffer, getBuffer, Py_ssize_t, buffer flags, etc.
 """
 from nimic.nimpy.py_types import *      # noqa: F401,F403
 from nimic.nimpy.raw_buffers import *   # noqa: F401,F403
+
+PyObject = object
